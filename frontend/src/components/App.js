8d8373553ff64a2af8f9a0dfe7e6c1bbf0272968
AdamKogut <akogut@purdue.edu>
2019-01-31 02:47:42 -0500

    Add tabs and create account button

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import {Route, Switch} from 'react-router-dom';
-import Header from './header/Header.js';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import {purple, green} from '@material-ui/core/colors';
+import Header from "./header/Header.js";
+import "./App.css";
 
 class App extends Component {
+  theme = createMuiTheme({
+    palette: {
+      primary: purple,
+      secondary: green,
+    },
+    status: {
+      danger: 'orange',
+    },
+  });
+
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider theme={this.theme}>
         <Header />
         <Switch>
-          <Route exact path='/' render = {()=><div>home</div>} />
-          <Route path='/saved' render ={()=><div>saved</div>}/>
-          <Route path='/grocery' render ={()=><div>grocery</div>}/>
-          <Route path='/pantry' render ={()=><div>pantry</div>}/>
-          <Route path='/planning' render ={()=><div>planning</div>}/>
+          <Route exact path="/" render={() => <div>home</div>} />
+          <Route path="/saved" render={() => <div>saved</div>} />
+          <Route path="/grocery" render={() => <div>grocery</div>} />
+          <Route path="/pantry" render={() => <div>pantry</div>} />
+          <Route path="/planning" render={() => <div>planning</div>} />
         </Switch>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
-import {Route, Switch} from 'react-router-dom';
-
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import { AppBar, Toolbar, Tabs, Tab, Button } from "@material-ui/core";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tabValue: 0
+    };
+  }
+
+  handleTabChange = (event, newValue) => {
+    this.setState({ tabValue: newValue });
+  };
+
   render() {
     return (
-      <div>
-      
-      </div>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Tabs
+            value={this.state.tabValue}
+            onChange={this.handleTabChange}
+            variant="scrollable"
+            scrollButtons="auto"
+          >
+            <Tab label="Home" value={0} />
+            <Tab label="Grocery List" value={1} />
+            <Tab label="Pantry" value={2} />
+            <Tab label="Meal Planning" value={3} />
+            <Tab label="Saved Recipes" value={4} />
+          </Tabs>
+          <Button
+            onClick={this.handleLogin}
+            style={{ position: "absolute", right: "10px" }}
+          >
+            Create Account/Login
+          </Button>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
