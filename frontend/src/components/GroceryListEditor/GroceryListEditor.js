d606b4c3ef9c7e2b67078986eb74375541f29735
Samuel Ingram <samingram22@gmail.com>
2019-02-27 12:42:50 -0500

    Create Initial Grocery List UI

---
+++ frontend/src/components/GroceryListEditor/GroceryListEditor.js
@@ -0,0 +1,177 @@
+import React from 'react';
+import './GroceryListEditor.css';
+
+import { Modal, Paper, Button, TextField } from '@material-ui/core';
+import Loader from "../Loader/Loader";
+import { connect } from "react-redux";
+import ConfirmationDialog from '../ConfirmationDialog/ConfirmationDialog';
+import AlertDialog from "../AlertDialog/AlertDialog";
+import Axios from "axios/index";
+
+class GroceryListEditor extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      listEdit: props.list,
+      closed: true,
+      warningIsOpen: false
+    };
+
+    this.noChangesAlert = React.createRef();
+  }
+
+  handleClose = () => {
+    // Check for unsaved notes
+    if (this.props.list !== this.state.listEdit) {
+      this.setState({
+        warningIsOpen: true
+      });
+
+      return;
+    }
+
+    // close the modal
+    this.props.onClose();
+    this.setState({
+      recipeEdit: {},
+      closed: true,
+      warningIsOpen: false
+    });
+  };
+
+  parseInput = (text) => {
+    let ingredientsList = text.split("\n").filter(function (el) {
+      return el !== "";
+    });
+
+    ingredientsList = ingredientsList.join("\n");
+
+    this.setState({
+      listEdit: {
+        ...this.state.listEdit,
+        ingredients: ingredientsList
+      }
+    });
+  };
+
+  saveList = () => {
+
+  };
+
+  render () {
+    // If this has just been opened, initialize stuff
+    if (this.props.list && this.state.closed) {
+      this.setState({
+        closed: false,
+        listEdit: this.props.list,
+      });
+
+      return null;
+    }
+
+    let content;
+    if (this.state.listEdit) {
+      content = (
+        <div>
+          <TextField
+            id={'edit-list-modal-title'}
+            onChange={(event) => {
+              this.setState({
+                listEdit: {
+                  ...this.state.listEdit,
+                  title: event.target.value
+                }
+              });
+            }}
+            value={this.state.listEdit.title}
+            fullWidth
+            label={'List Title'}
+          />
+
+          <div id={"edit-list-modal-toolbar"}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.saveList}
+            >
+              Save
+            </Button>
+
+            <Button
+              variant="contained"
+              onClick={this.handleClose}
+            >
+              Cancel
+            </Button>
+          </div>
+          <br />
+
+          <div id={'edit-list-modal-description'}>
+            <em>Enter Ingredients as one per line</em>
+            <br />
+            <br />
+
+            <TextField
+              onChange={(event) => {
+                this.parseInput(event.target.value);
+              }}
+              className={'edit-list-modal-textarea'}
+              value={this.state.listEdit.ingredients}
+              multiline
+            />
+          </div>
+        </div>
+      );
+    } else {
+      content = <Loader/>
+    }
+
+    return (
+      <div>
+        <Modal
+          open={this.props.list !== null}
+          onClose={this.handleClose}
+        >
+          <Paper className={'modal-container'}>
+            {content}
+          </Paper>
+        </Modal>
+
+        <ConfirmationDialog
+          isOpen={this.state.warningIsOpen}
+          title={"Exit?"}
+          text={"You have unsaved changes for this list. Exit anyways?"}
+          agreeText={"Yes"}
+          disagreeText={"No"}
+          onClose={() => {
+            this.setState({
+              warningIsOpen: false
+            });
+          }}
+          onAgree={() => {
+            // close the modal
+            this.props.onClose();
+            this.setState({
+              listEdit: {},
+              closed: true,
+              warningIsOpen: false
+            });
+          }}
+        />
+
+        <AlertDialog
+          title={"Alert"}
+          text={"There are no changes to be saved for the current list"}
+          ref={this.noChangesAlert}
+        />
+      </div>
+    );
+  }
+}
+
+function mapStatesToProps({ auth }) {
+  return { auth: auth };
+}
+
+export default connect(mapStatesToProps)(GroceryListEditor);
--- frontend/src/components/grocery/mainGrocery.js
+++ frontend/src/components/grocery/mainGrocery.js
@@ -1,11 +1,124 @@
 import React, { Component } from "react";
 import "./mainGrocery.css";
 
+import GroceryList from "../GroceryList/GroceryList";
+import BodyContainer from "../BodyContainer/BodyContainer";
+import ConfirmationDialog from "../ConfirmationDialog/ConfirmationDialog";
+import GroceryListEditor from "../GroceryListEditor/GroceryListEditor";
+
 class mainGrocery extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      groceryLists: [],
+      listToDelete: null,
+      listToEdit: null,
+      deleteDialogIsOpen: false,
+      displayingEditModal: false
+    };
+  }
+
+  componentDidMount = () => {
+    this.getGroceryLists();
+  };
+
+  getGroceryLists = () => {
+    this.setState({
+      groceryLists: [{
+        title: "Test List",
+        ingredients: [
+          "a banana",
+          "some bread",
+          "asdfasdf?"
+        ]
+      }, {
+        title: "Second Test List",
+        ingredients: [
+          "a banana",
+          "some bread",
+          "asdfasdf?"
+        ]
+      }]
+    });
+  };
+
+  onEditList = (list) => {
+    this.setState({
+      listToEdit: list,
+      displayingEditModal: true
+    });
+  };
+
+  onEditClose = () => {
+    this.setState({
+      listToEdit: null,
+      displayingEditModal: false
+    });
+  };
+
+  onDeleteList = (name) => {
+    this.setState({
+      listToDelete: name,
+      deleteDialogIsOpen: true
+    });
+  };
+
+  deleteList = (name) => {
+    this.setState({
+      deleteDialogIsOpen: false
+    });
+  };
+
   render() {
+    const groceryLists = [];
+
+    if (this.state.groceryLists.length === 0) {
+      groceryLists.push(
+        <div>
+          Create some Grocery Lists to see them here!
+        </div>
+      );
+    } else {
+      for (let i = 0; i < this.state.groceryLists.length; i++) {
+        groceryLists.push(
+          <GroceryList
+            list={this.state.groceryLists[i]} key={"groceryList" + i}
+            onUpdate={this.getGroceryLists}
+            onDelete={this.onDeleteList}
+            onEdit={this.onEditList}
+          />
+        );
+      }
+    }
+
     return (
       <div className='BigDivArea'>
-        Grocery
+        <BodyContainer>
+          {groceryLists}
+        </BodyContainer>
+
+        <GroceryListEditor
+          list={this.state.displayingEditModal ? this.state.listToEdit : null}
+          onClose={this.onEditClose}
+          onSave={this.onSaveEdit}
+        />
+
+        <ConfirmationDialog
+          isOpen={this.state.deleteDialogIsOpen}
+          title={"Delete List"}
+          text={"Are you sure you want to delete " + this.state.listToDelete + "?"}
+          agreeText={"Yes"}
+          disagreeText={"No"}
+          onClose={() => {
+            this.setState({
+              deleteDialogIsOpen: false
+            });
+          }}
+          onAgree={() => {
+            this.deleteList(this.state.listToDelete);
+          }}
+        />
       </div>
     );
   }
