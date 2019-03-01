03eda6393fbd822b1709f90a90aa3ea5e8f9844f
jadenz98 <zerbe@purdue.edu>
2019-02-28 20:54:50 -0500

    Can now sort by ratings

--- backend/routes/ratings.js
+++ backend/routes/ratings.js
@@ -54,7 +54,7 @@ router.post('/', function(req,res,next){
       var ratings = document.ratings;
       var recipeId = req.body.recipeId;
       var rating = req.body.rating;
-      console.log(obj);
+      //console.log(obj);
       ratings[recipeId]=rating;
       console.log(ratings);
       myDBO.collection("users").updateOne({googleId: user},{$set: {ratings: ratings}}, function(err, result){
