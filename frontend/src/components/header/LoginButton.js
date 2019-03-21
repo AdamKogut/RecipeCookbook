4e6bc3b30b4ebce27d7b54cac5c6a9e577c5f92a
jadenz98 <zerbe@purdue.edu>
2019-03-21 17:53:37 -0400

    testing stuff

--- frontend/src/components/header/LoginButton.js
+++ frontend/src/components/header/LoginButton.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import {Button} from '@material-ui/core';
+//import firebase from 'firebase';
 
 class LoginButton extends Component {
   handleLogin=()=>{
     window.location.href = "http://localhost:8080/auth/google";
   };
 
+  handleGoogleLogin() {
+    loginWithGoogle()
+      .catch(function (error) {
+        alert(error);
+        localStorage.removeItem(firebaseAuthKey);
+      });
+    localStorage.setItem(firebaseAuthKey, "1");
+  }
+
+  firebaseAuth().onAuthStateChanged(user => {
+    if(user) {
+      console.log("User signed in: ", JSON>stringify(user));
+      localStorage.removeItem(firebaseAuthKey);
+      this.props.history.push("/");
+    }
+  });
   render() {
     return (
       <Button
